de8b7f915964a3bbf3bcbba3f2951aeee2422a93
jbgaela <jbgaela@gmail.com>
2021-12-12 17:35:08 +0800

    Added middlware

--- routes/productRoutes.js
+++ routes/productRoutes.js
@@ -1,14 +1,14 @@
 import express from 'express'
 const router = express.Router()
-import {
-  getProducts,
-  getProductById
-} from '../controllers/productController.js'
+import {getProducts, getProductById } from '../controllers/productController.js'
 
 // @desc    Fetch all products
 // @route   GET /api/products
 // @access  Public
 router.route('/').get(getProducts)
 router.route('/:id').get(getProductById)
+
+
+
 
 export default router
